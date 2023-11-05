a7e4b8ff37ea721fa37e8949ac04f737b477b1c3
shashank-h-s <shashankhsappu@gmail.com>
2023-11-05 22:49:32 +0530

    login page

---
+++ index.js
@@ -0,0 +1,20 @@
+const wrapper = document.querySelector(".wrapper");
+const loginLink = document.querySelector(".login-link");
+const registerLink = document.querySelector(".register-link");
+const btnPopup = document.querySelector(".btnLogin-popup");
+const closeIcon = document.querySelector(".icon-close");
+registerLink.addEventListener("click", () => {
+  wrapper.classList.add("active");
+});
+
+loginLink.addEventListener("click", () => {
+  wrapper.classList.remove("active");
+});
+
+btnPopup.addEventListener("click", () => {
+  wrapper.classList.add("active-btnPopup");
+});
+
+closeIcon.addEventListener("click", () => {
+  wrapper.classList.remove("active-btnPopup");
+});
